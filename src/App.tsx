f385284cecd6a7dc27c53c5c7473d07916903cdc
hiten-ahir <hitenb1121@gmail.com>
2025-07-12 10:31:42 +0530

    Initial and final commit

---
+++ src/App.tsx
@@ -0,0 +1,37 @@
+import { useEffect, useState } from 'react'
+import axios from 'axios';
+import './App.css'
+
+function App() {
+  type Quote = { sentence: string, character: { name: string } }
+  const [quote, setQuote] = useState<Quote>({
+    sentence: "",
+    character: {
+      name: ""
+    }
+  })
+
+  useEffect(() => {
+    getQuote()
+  }, [])
+
+  const getQuote = async () => {
+    axios.get('https://api.gameofthronesquotes.xyz/v1/random')
+      .then((res) => {
+        setQuote(res.data)
+      })
+      .catch((err) => console.error(err))
+  }
+
+  return (
+    <>
+      <div className='quote-box'>
+        <img src="/favicon.jpeg" alt="Game of Thrones" />
+        <p className='quote-text'>{quote?.sentence}</p>
+        <p className="quote-author"> — {quote?.character?.name}</p>
+      </div>
+    </>
+  )
+}
+
+export default App
